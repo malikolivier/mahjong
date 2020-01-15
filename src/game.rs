78531471a915787d2a8bf6615a2c8be3f6cabb6c
Malik Olivier Boussejra <malik@boussejra.com>
2020-01-15 18:17:30 +0100

    Fix tsumo point

--- src/game.rs
+++ src/game.rs
@@ -227,7 +227,7 @@ impl Game {
 
         for i in 0..13 {
             for p in 0..4 {
-                let tsumohai_i = (break_point + i + p * 14) % 136;
+                let tsumohai_i = (break_point + i + p * 13) % 136;
                 if let Some(tsumohai) = self.yama[tsumohai_i] {
                     self.players[p].te.hai.insert(tsumohai);
                     self.yama[tsumohai_i] = None;
